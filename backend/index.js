30c2891261ad6862f8703051a7e18bc06eaa23bf
Murphey Osmundson <mosmundson88@gmail.com>
2024-05-09 14:23:04 -0400

    refactor

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,33 @@
 import express from "express";
 import { PORT, MongoDBURL } from "./config.js"
 import mongoose from "mongoose"
+import { Book } from "./models/models.js";
+import booksRoute from'./routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
+
+// Middleware for parsing JSON request bodies
+app.use(express.json());
+
+// Middleware for handling CORS Policy
+
+// Allow all origins
+app.use(cors())
+
+// Allow custom origins
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}))
 
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send('Welcome To MERN Stack')
 });
 
-// Route for saving a new book
-app.post('/books', async (request, response) => {
-    try {
-        if (
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'Please enter all required fields: title, author, publish year',
-            });
-        }
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-
-        const book = await Book.create(newBook);
-
-        return response.status(201).send(book)
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
+app.use('/books', booksRoute);
 
 mongoose
     .connect(MongoDBURL)
